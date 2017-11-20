ed7fae232b1b8b0cf7b40794df9b183f8ccb446a
Mark Roger Patanga <marknit363@gmail.com>
2017-11-20 19:30:05 -0200

    Atualização Procfile

--- src/main/java/com/sing/api/config/AuthorizationServerConfig.java
+++ src/main/java/com/sing/api/config/AuthorizationServerConfig.java
@@ -7,6 +7,7 @@ import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
@@ -20,7 +21,7 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 import com.sing.api.config.token.CustomTokenEnhancer;
 
-//@Profile("oauth-security")
+@Profile("oauth-security")
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
